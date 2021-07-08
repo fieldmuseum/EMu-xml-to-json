113b72657f53dc2afe99bb1b8aa6cf084a83e284
magpiedin <magpiedin@gmail.com>
2021-07-07 21:14:39 -0500

    cleanup

--- emu_xml_to_json.py
+++ emu_xml_to_json.py
@@ -5,15 +5,14 @@ import convert_xml as cvx
 from decouple import config
 from datetime import date, datetime
 
-# If xml_check is ok, Read in XML file
-# try test.find("is well-formed") > -1:
 xml_in = config('INPUT_PATH') + config('INPUT_XML')
 
+# If xml_check is ok, Read in XML file:
+# Else output error/exception-log
 try:
     cvx.xml_to_json(xml_in)
 
 except Exception as e:
-    # log_encode = 'error - cannot perform charset normalization: ' + str(e)
     logs = open(config('OUTPUT_PATH') + 'xml_log_' + str(date.today()) + '.txt', 'a')
     logs.write(str(datetime.now()) + ' - File Input: ' + xml_in + ' : Error : ' + str(e) + '\n')
     logs.close()
@@ -22,4 +21,3 @@ except Exception as e:
     chx.fix_xml_encode(xml_in)
     # cx.check_xml_encode(xml_in)
     # cx.check_xml_form(xml_in)
-    
