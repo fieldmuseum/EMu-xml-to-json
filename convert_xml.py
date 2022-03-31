4541b07dcaedc1015c7509110e3a45b0aad12169
magpiedin <magpiedin@gmail.com>
2022-03-31 01:44:41 -0500

    add exh-flag and people

--- convert_xml.py
+++ convert_xml.py
@@ -42,6 +42,7 @@ def get_group_tuple(tup_field, tup_group_field, emu_map, map_condition, group_al
 
 
     if tup_field.tag in m_emu_group:
+        # print('0 - get Group-tuple: ' + str(tup_field.tag) + ' = ' + str(tup_field.text))
 
         e_json_field = emu_map.query('emu_group == @tup_field.tag')['json_field'].values
         e_json_group = emu_map.query('emu_group == @tup_field.tag')['json_container'].values
@@ -51,8 +52,6 @@ def get_group_tuple(tup_field, tup_group_field, emu_map, map_condition, group_al
 
         if len(e_json_group) > 0:
             str_json_group = str(e_json_group[0])
-        # if len(e_json_con_group) > 0:
-        #     str_json_con_group = str(e_json_con_group[0])
 
         for sib_key in tup_group_field:
 
@@ -62,16 +61,12 @@ def get_group_tuple(tup_field, tup_group_field, emu_map, map_condition, group_al
                 group_temp_dict[str(t_g_json_field)[2:-2]] = str(sib_key.text)
 
 
-
             elif sib_key.text not in ["",[], None]:
                 sib_key_str = str(emu_map.query('emu == @sib_key.tag')['json_field'].values)[2:-2]
                 if sib_key.tag in t_emu_field:
                     group_temp_dict[sib_key_str] = str(sib_key.text)
-                # else:
-                #     group_temp_dict[sib_key_str] = None
-
+                
         if len(t_json_con_group) > 0:
-            # e_json_con_group = map_condition.query('if_field1 == @group_field.tag')['json_container'].values
             str_t_json_con_group = str(e_t_json_con_group[0])
 
 
@@ -79,13 +74,9 @@ def get_group_tuple(tup_field, tup_group_field, emu_map, map_condition, group_al
 
             group_all[str_t_json_con_group].append(group_temp_dict.copy())
 
-            # return group_all
-
         elif str_json_group in emu_json_groups:
 
             group_all[str_json_group].append(group_temp_dict.copy())
-
-            # return group_all
         
         elif len(tup_group_field) == 1:
 
@@ -102,18 +93,21 @@ def get_group_nontuple(tuple_group, tup_field, emu_map, map_condition, group_all
     t_emu_field = emu_map.query('emu_group == @tup_field.tag')['emu'].values
 
     for group_field in tuple_group:
+        # print('0.1.2-1 - get group non-tup: ' + str(group_field.tag) + ' = ' + str(group_field.text))
             
+        e_json_field = emu_map.query('emu == @group_field.tag')['json_field'].values
+        e_json_group = emu_map.query('emu == @group_field.tag')['json_container'].values
+
+        e_emu_if_field = map_condition.query('if_field1 == @group_field.tag')['if_field1'].values
+        e_emu_if_value = map_condition.query('if_field1 == @group_field.tag')['if_value1'].values
+        e_emu_then_field = map_condition.query('if_field1 == @group_field.tag')['then_field'].values
+        e_json_con_field = map_condition.query('if_field1 == @group_field.tag')['json_field'].values
+        e_json_con_value = map_condition.query('if_field1 == @group_field.tag')['static_value'].values
+        e_json_con_group = map_condition.query('if_field1 == @group_field.tag')['json_container'].values
+
+
         if group_field.tag in t_emu_field:
-
-            e_json_field = emu_map.query('emu == @group_field.tag')['json_field'].values
-            e_json_group = emu_map.query('emu == @group_field.tag')['json_container'].values
-
-            e_emu_if_field = map_condition.query('if_field1 == @group_field.tag')['if_field1'].values
-            e_emu_if_value = map_condition.query('if_field1 == @group_field.tag')['if_value1'].values
-            e_emu_then_field = map_condition.query('if_field1 == @group_field.tag')['then_field'].values
-            e_json_con_field = map_condition.query('if_field1 == @group_field.tag')['json_field'].values
-            e_json_con_value = map_condition.query('if_field1 == @group_field.tag')['static_value'].values
-            e_json_con_group = map_condition.query('if_field1 == @group_field.tag')['json_container'].values
+            # print('0.1.2-1.1 - group non-tup ' + str(group_field.tag) + ' in t-emu-field ' + str(t_emu_field))
 
             group_temp_dict = {}
 
@@ -122,18 +116,20 @@ def get_group_nontuple(tuple_group, tup_field, emu_map, map_condition, group_all
             if len(e_json_con_group) > 0:
                 str_json_con_group = str(e_json_con_group[0])
 
+            
+            # # # # # # # Nested-Ref-tuple/atom-to-1 -- withOUT conditional mapping
+            # e.g. - LocTemporaryLocationRef.irn --> fmnh:onExhibitOffsiteId
+            if e_json_group not in emu_json_groups and len(str(e_json_field)[2:-2]) > 0:
+                # print('2 - Nest-Ref/Table-to-1: ' + str(group_field.tag) + ' = ' + str(group_field.text)) 
 
-            # # # # # # # REF/TABLE-to-1 -- withOUT conditional mapping
-            # e.g. ColCollectionEventRef.ColSiteRef.PolPD1 --> dwc:country
-            if group_field.tag not in e_emu_if_field: # and group_field.tag in single_dict.keys()
+                group_all[str(e_json_field)[2:-2]] = group_field.text  # # # # use += instead? 
+                # print("atoms : " + str(group_field.tag) + ' = ' + str(group_field.text))
 
-                if e_json_group not in emu_json_groups:
-
-                    group_all[str(e_json_field)[2:-2]] = group_field.text  # # # # use += instead?                             
 
             # # # # # # # REF/TABLE-to-1 -- WITH conditional mapping [all currently to multi-value h2i field]
             # e.g. PriAccessionNumberRef.AccAccessionNo --> cd:identifier + cd:identifierType --> 'accession number'
             elif e_emu_if_value == "NOT NULL":  # and str_json_con_group is not None:
+                # print('3 - Ref/Table-to-1 + CON: ' + str(group_field.tag) + ' = ' + str(group_field.text)) 
 
                 group_temp_dict[str(e_json_field)[2:-2]] = str(group_field.text)
                 group_temp_dict[str(e_json_con_field)[2:-2]] = str(group_field.text)
@@ -142,10 +138,39 @@ def get_group_nontuple(tuple_group, tup_field, emu_map, map_condition, group_all
                     group_temp_dict[str(key)] = str(val)
 
                 group_all[str_json_con_group].append(group_temp_dict.copy())
+
+            
+            # # # # # # # Nested-Ref-tuple/atom-to-group -- withOUT conditional mapping
+            # e.g. - PriAccessionNumberRef.AccCollectorRef_tab.AccColl...NamFullName --> people[dwc:recordedBy]
+            elif e_json_group in emu_json_groups and len(str(e_json_field)[2:-2]) > 0:
+                # print('4 - Nest-Ref/Table-to-group: ' + str(group_field.tag) + ' = ' + str(group_field.text)) 
+
+                str_json_group = str(e_json_group)[2:-2]
+                group_temp_dict = {}
+
+                group_temp_dict[str(e_json_field)[2:-2]] = str(group_field.text)
+
+                group_all[str_json_group].append(group_temp_dict.copy())
             
             group_temp_dict.clear()
 
-            return group_all
+
+        # # # # # # # REF/TABLE-to-1 -- withOUT conditional mapping
+        # e.g. ColCollectionEventRef.ColSiteRef.PolPD1 --> dwc:country        
+        elif group_field.tag not in e_emu_if_field: # and group_field.tag in single_dict.keys()
+            # print('1 - Ref/Table-to-1: ' + str(group_field.tag) + ' = ' + str(group_field.text))
+
+            if e_json_group not in emu_json_groups and len(str(e_json_field)[2:-2]) > 0:
+
+                group_all[str(e_json_field)[2:-2]] = group_field.text  # # # # use += instead?
+            
+                # return group_all
+
+        else:
+            # print('0.1.2-1.2 - group non-tup ' + str(group_field.tag) + ' has no t-emu-field ' + str(t_emu_field))
+            return
+    
+    return group_all
 
 
 def xml_to_json(xml_input, emu_xml_out=False):
@@ -199,6 +224,7 @@ def xml_to_json(xml_input, emu_xml_out=False):
         tup_fields = get_tup_fields(tuple1) 
 
         for tup_field in tup_fields:
+            # print('field:  ' + str(tup_field.tag) + ' == ' + str(tup_field.text))
 
             # Get emu-xml-field's corresponding h2i field + info
             t_emu_field = emu_map.query('emu == @tup_field.tag')['emu'].values
@@ -223,6 +249,8 @@ def xml_to_json(xml_input, emu_xml_out=False):
 
                 # # # # # # #
                 # 1-to-1 -- withOUT conditional mapping
+                # print('0.0 - get single: ' + str(tup_field.tag) + ' = ' + str(tup_field.text) + ' for grp ' + str(t_json_field))
+                # print('...and current group = ' + str(group_all))
                 if str(t_json_field)[2:-2] in single_dict.keys():
 
                     group_all[str(t_json_field[0])] = tup_field.text
@@ -231,6 +259,7 @@ def xml_to_json(xml_input, emu_xml_out=False):
                 # # # # # # #
                 # 1-to-Group -- WITH conditional mapping [all currently to multi-value h2i field]
                 elif "NOT NULL" in t_emu_if_value and t_json_group is not None:
+                    # print('0.0 - get 1:Group: ' + str(tup_field.tag) + ' = ' + str(tup_field.text))
 
                     str_json_con_group = str(t_json_con_group[0])
                     str_json_group = str(t_json_group[0])
@@ -249,6 +278,7 @@ def xml_to_json(xml_input, emu_xml_out=False):
             # # # # # # #
             # EMu REF & TABLE fields  (where tup_field.tag is actually a value in emu_map['emu_group'])
             elif tup_field.tag in emu_map['emu_group'].values and tup_field.tag is not None:
+                # print('0.1 - get Ref-tab: ' + str(tup_field.tag) + ' = ' + str(tup_field.text))
 
                 check_tuples = etree.XPath("./tuple")
                 has_tuples = check_tuples(tup_field)
@@ -257,6 +287,7 @@ def xml_to_json(xml_input, emu_xml_out=False):
                 # TUPLES - handle these first 
                 # [separately from Ref or Table fields withOUT tuples/grouped EMu-fields]
                 if len(has_tuples) > 0:
+                    # print('0.1.1 - get Ref-tab+tup: ' + str(tup_field.tag) + ' = ' + str(tup_field.text))
 
                     for tuple1_1 in has_tuples:
 
@@ -269,6 +300,9 @@ def xml_to_json(xml_input, emu_xml_out=False):
 
                 # if NOT a tuple:
                 else:
+                    # print('0.1.2 - get Ref-tab: ' + str(tup_field.tag) + ' = ' + str(tup_field.text))
+                    check_tuples = etree.XPath("./tuple")
+                    has_tuples = check_tuples(tup_field)
 
                     group_all = get_group_nontuple(tuple_group, tup_field, emu_map, map_condition, group_all)
 
